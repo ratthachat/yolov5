eea0115dd127770600fe8352127d5ffd1afce807
Ratthachat (Jung) <56621342+ratthachat@users.noreply.github.com>
2020-06-29 09:42:33 +0700

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -520,7 +520,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
                     labels[:, 1] = 1 - labels[:, 1]
 
             # random up-down flip
-            ud_flip = False
+            ud_flip = True
             if ud_flip and random.random() < 0.5:
                 img = np.flipud(img)
                 if nL:
